e129a40b036d2554ebb133863e1bc15920e23a26
jemiddle18 <jemiddle18@gmail.com>
2022-04-14 11:24:20 -0400

    Add files via upload

---
+++ Port_Scanning.py
@@ -0,0 +1,12 @@
+import socket
+
+for port in range(1,4000): # Loop to check if a port is open. Looping throughout the designated range.
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Open up the socket and a stream.
+    results = sock.connect_ex(("Host IP", port)) # Connecting to the particular host to scan.
+
+    #If and else statement to determine if the ports are open or closed.
+    if results == 0:
+        print("Port " + str(port) + " is open")
+    else:
+        print("Port " + str(port) + " is closed")
+    sock.close()
